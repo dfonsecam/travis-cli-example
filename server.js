c13c4d10e238f1f1f2d82737306e5b66da61509d
dfonsecam <tuma05@gmail.com>
2017-07-06 11:34:51 -0600

    msj

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = require('express')();
 app.get('/', (req, res) => {
   res.send('Travis-cli example online!');
 });
+app.get('/test', (req, res) => {
+  res.send('run test!');
+});
 const server = app.listen(8080, () => {
   const host = server.address().address;
   const port = server.address().port;
